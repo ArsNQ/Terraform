07e6cd42f9fb413e1a2048e193e07999eef84c3e
Quentin Tridon <quentin.tridon@bell.ca>
2023-02-09 19:41:25 -0500

    add regions + zone in simpleInstance.tf

--- GCP/simpleInstance/simpleInstance.tf
+++ GCP/simpleInstance/simpleInstance.tf
@@ -1,12 +1,13 @@
 provider "google" {
-  version = "3.0.0"
+  version = "4.52.0"
   project = "" #GCP project name.
   region  = "" #GCP region name.
 }
 
-resource "google_compute_instance" "example" {
+resource "google_compute_instance" "default" {
   name         = "" #GCP instance name.
   machine_type = "" #GCP machine type.
+  zone         = "" #GCP zone type.
 
   boot_disk {
     initialize_params {
