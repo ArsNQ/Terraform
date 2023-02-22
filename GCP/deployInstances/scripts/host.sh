f2e398d2249d43cdd6177ab96e438e8dc9ddacfc
Quentin Tridon <q.tridon@gmail.com>
2023-02-22 04:05:05 -0500

    Update host.sh

--- GCP/deployInstances/scripts/host.sh
+++ GCP/deployInstances/scripts/host.sh
@@ -7,19 +7,19 @@ num_master=0
 num_worker=0
 
 # Écrit les adresses IP des masters dans le fichier Ansible hosts.
-echo "[k8sMasters]" > /Users/eq29753/Documents/Github/deployKubernetes/containerd/hosts
+echo "[k8sMasters]" > /deployKubernetes/containerd/hosts
 for IP_ADDRESSES_MASTER in $IP_ADDRESSES_MASTER; do
     hostname_master="kubernetes-master-$(printf "%02d" $num_master)"
-    echo "${hostname_master} ansible_host=$IP_ADDRESSES_MASTER ansible_user=ubuntu" >> /Users/eq29753/Documents/Github/deployKubernetes/containerd/hosts
+    echo "${hostname_master} ansible_host=$IP_ADDRESSES_MASTER ansible_user=ubuntu" >> /deployKubernetes/containerd/hosts
     num_master=$((num_master + 1))
 done
 
 # Écrit les adresses IP des workers dans le fichier Ansible hosts.
-echo "[k8sWorkers]" >> /Users/eq29753/Documents/Github/deployKubernetes/containerd/hosts
+echo "[k8sWorkers]" >> /deployKubernetes/containerd/hosts
 for IP_ADDRESSES_WORKERS in $IP_ADDRESSES_WORKERS; do
     hostname_worker="kubernetes-worker-$(printf "%02d" $num_worker)"
-    echo "${hostname_worker} ansible_host=$IP_ADDRESSES_WORKERS ansible_user=ubuntu" >> /Users/eq29753/Documents/Github/deployKubernetes/containerd/hosts
+    echo "${hostname_worker} ansible_host=$IP_ADDRESSES_WORKERS ansible_user=ubuntu" >> /deployKubernetes/containerd/hosts
     num_worker=$((num_worker + 1))
 done
 
-echo "ip_master: $IP_ADDRESSES_MASTER" >> /Users/eq29753/Documents/Github/deployKubernetes/containerd/setupMaster/vars/main.yml
+echo "ip_master: $IP_ADDRESSES_MASTER" >> /deployKubernetes/containerd/setupMaster/vars/main.yml
